13be33d68e4289e4f86a5be226ed46fe56c9c18f
Chris Kiriakou <chris-kiriakou@web.de>
2025-09-22 14:13:45 +0200

    Simple gin sample with go-template for showcasing & testing

--- app/src/main.go
+++ app/src/main.go
@@ -1,13 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-  router := gin.Default()
-  router.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong",
-    })
-  })
-  router.Run() // listens on 0.0.0.0:8080 by default
+	// gin
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/index", func(c *gin.Context) {
+		data := struct {
+			Title string
+			Items []string
+		}{
+			Title: "My page",
+			Items: []string{
+				"My photos",
+				"My blog",
+			},
+		}
+		c.HTML(http.StatusOK, "index.tmpl", data)
+	})
+
+	router.Run() // listens on 0.0.0.0:8080 by default
 }
